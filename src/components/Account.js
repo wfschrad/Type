4dfad912b84a6bdcd11db7100ff282f64f900884
William Schrader <wfschrad@gmail.com>
2020-06-23 17:18:18 -0700

    Minor adjustments.

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Account() {
     return (
         <div>
-            <h1>This is where user will be able to configure settings or change content</h1>
+            <h1>This is where User will be able to configure settings or change content</h1>
         </div>
     )
 }
